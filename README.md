02df83abddac9965c125c1529e6e81bad2bec0f1
Malak-Atef <79705774+Malak-Atef@users.noreply.github.com>
2021-04-12 23:21:44 +0200

    Weather-Journal-App-2021

---
+++ README.md
@@ -0,0 +1,12 @@
+Project Objective
+This project was about working with Web APIs and asynchronous code, creating a web app that uses OpenWeatherMap API to get weather based on the user's location, and dynamically update UI.
+
+Building
+To get the project up and running I followed these steps:
+
+1-Setting up project environment, making sure I have Node and packages installed, and included in my server.js file.
+2-Added POST and GET routes to ensure correct retrieval of data from the server.
+3-Acquired API credentials from OpenWeatherMap website.
+4-Created async functions to fetch weather data and store it on my local server.
+5-Set up a function that updated UI dynamically.
+
---
+++ server.js
@@ -0,0 +1,47 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require ('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+/** Listen Port */
+const port = 4000;
+
+// Setup Server
+const server = app.listen(port, listening);
+function listening(){
+    console.log("server is running");
+    console.log(`running on localhost: ${port}`);
+};
+
+// Require Express to run server and routes
+app.get('/all' , (request , response) =>{
+        response.send(projectData);
+});
+ //Post Data
+app.post('/postData',(request, response) =>{
+ //Post Data Now
+    projectData={
+        temp: request.body.temp,
+        content: request.body.content,
+        data: request.body.data,
+     
+    };
+    response.send(projectData);
+})
